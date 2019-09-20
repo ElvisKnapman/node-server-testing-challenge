5d37e096fe7721b6e15a93453d1c52e7c3448357
Elvis Knapman <elvis@elvisknapman.com>
2019-09-19 20:18:41 -0400

    :sparkles: wrote handler for /drivers/ GET route

--- drivers/driversRoute.js
+++ drivers/driversRoute.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
 
+// model
+const Drivers = require("./drivers-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const drivers = await Drivers.find();
+
+    if (drivers) {
+      res.status(200).json(drivers);
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server encountered error retrieving drivers" });
+  }
+});
+
 module.exports = router;
